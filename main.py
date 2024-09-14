cf176e70561a7f2c7f83bfabfac2664b3ac7b910
VigneshvickyData <candycrushvicky7@gmail.com>
2024-09-14 18:15:21 +0530

    main.py and logger

---
+++ main.py
@@ -0,0 +1,8 @@
+from mlProject import logger
+#from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+#from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+#from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+#from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+#from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+logger.info("welcome to our custom log")
