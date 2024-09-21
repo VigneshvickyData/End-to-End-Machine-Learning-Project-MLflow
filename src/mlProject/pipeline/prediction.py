0b45df51c1f75899db15d535a8e31a4dd12ff4df
VigneshvickyData <candycrushvicky7@gmail.com>
2024-09-21 14:18:50 +0530

    artifacts & pipeline  add

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -3,16 +3,13 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-
-
 class PredictionPipeline:
     def __init__(self):
-        self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
-
+        # Use Path.resolve() to ensure absolute path
+        model_path = Path('artifacts/model_trainer/model.joblib').resolve()
+        self.model = joblib.load(model_path)
     
     def predict(self, data):
         prediction = self.model.predict(data)
-
         return prediction
-    
-    ## D:\End-to-End-Machine-Learning-Project-MLflow\artifacts\model_trainer\model.joblib 
+
